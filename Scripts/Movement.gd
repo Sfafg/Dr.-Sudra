247dda5b05eaeac4e95038f23354beaec43544f6
Sławek <slawekzdunek2.3.6@gmail.com>
2023-10-30 20:15:58 +0100

    Animacja kroków

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -2,20 +2,23 @@ extends Sprite2D
 
 @export var maxSpeed : float = 2
 @export var mass : float = 2
+@export var stepFrequency : float = 10
+@export var stepInfluence : float = 0.4
 
 var velocity = Vector2(0,0)
-func _ready():
-	pass
-
-
+var stepTimer = 0
 func _process(delta):
 	var moveDirection = Vector2(0,0)
 	
 	moveDirection.x += float(Input.is_key_pressed(KEY_D)) - float(Input.is_key_pressed(KEY_A))
 	moveDirection.y += float(Input.is_key_pressed(KEY_S)) - float(Input.is_key_pressed(KEY_W))
-	moveDirection = moveDirection.normalized()
+	if(moveDirection.length() == 0):
+		stepTimer = 0
+	else:
+		moveDirection = moveDirection.normalized()
+		
 	
-	var targetVelocity = moveDirection * maxSpeed
+	var targetVelocity = moveDirection * maxSpeed * clamp(abs(sin(stepTimer * stepFrequency)) + 1 - stepInfluence,0,1)
 	var velocityDifferance = targetVelocity - velocity
 	
 	velocity += velocityDifferance / mass * delta / 0.016
@@ -23,4 +26,5 @@ func _process(delta):
 	
 	self.scale.x = -abs(self.scale.x) if velocity.x <= 0 else abs(self.scale.x)
 	self.position += velocity * delta / 0.016
+	stepTimer += delta
 	pass
