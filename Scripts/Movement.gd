e4e714b72c99a61773b71795b6c67d229049e530
Sławek <slawekzdunek2.3.6@gmail.com>
2023-11-03 19:37:34 +0100

    Poprawka zgodności z zasadami formatowania

--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -1,37 +1,22 @@
 extends Sprite2D
 
-@export var maxSpeed : float = 2
-@export var mass : float = 2
-@export var stepFrequency : float = 10
-@export_range(0.0,1.0) var stepAnimationInfluence : float = 0.4
-
+@export var max_speed = 7.0
+@export var mass = 2.0
 var velocity = Vector2(0,0)
-var stepAnimationTimer = 0
+
 func _process(delta):
 	# Pobieranie wejścia z klawiatury.
-	var moveDirection = Input.get_vector("left", "right", "up", "down")
-	# Jeśli gracz nie wysyła zadnego inputu,
-	# to zrestartuj animację kroku oraz oraz jej nie używaj.
-	var stepAnimation = 1
-	if(moveDirection.x == 0 && moveDirection.y == 0):
-		stepAnimationTimer = 0
-	else:
-		# Obliczanie animacji kroku na podstawie funkcji sinus i ograniczenie jej od 1 - stepInfluence do 1.
-		stepAnimation = sin(stepAnimationTimer * stepFrequency) * 0.5 + 0.5
-		stepAnimation = clamp(stepAnimation + 1 - stepAnimationInfluence, 1 - stepAnimationInfluence, 1)
-		stepAnimationTimer += delta
-		moveDirection = moveDirection.normalized()
+	var move_direction = Input.get_vector("left", "right", "up", "down")
 	
 	# Obliczanie prędkości, którą ma osiągnąć objekt.
-	var targetVelocity = moveDirection * maxSpeed
-	#targetVelocity *= stepAnimation
+	var target_velocity = move_direction.normalized() * max_speed
 	
-	# Siła potrzebna do skierowania postaci w kierunku targetVelocity.
-	var steeringForce = targetVelocity - velocity
+	# Siła potrzebna do skierowania postaci w kierunku target_velocity.
+	var steering_force = target_velocity - velocity
 
 	# Integracja przyspieszenia.
-	velocity += steeringForce / mass * delta * 60
-	velocity.limit_length(maxSpeed)
+	velocity += steering_force / mass * delta * 60
+	velocity.limit_length(max_speed)
 	
 	# Spraw by obiekt był skierowany w stronę kierunku ruchu.
 	self.scale.x = -abs(self.scale.x) if velocity.x <= 0 else abs(self.scale.x)
