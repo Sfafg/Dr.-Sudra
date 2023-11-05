43f42979ac4350790c792f98c61b41ab81ca5c50
Sławek <slawekzdunek2.3.6@gmail.com>
2023-11-05 18:04:50 +0100

    Poprawa movementu na CharacterBody2D

--- Scripts/Gamba.gd
+++ Scripts/Gamba.gd
@@ -9,10 +9,3 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):   
 		var randomPicker = RandomPicker.new()    
 		print(randomPicker.pick_random_item(items_array))
-
-
-
-
-
-
-
--- Scripts/Movement.gd
+++ Scripts/Movement.gd
@@ -1,32 +1,19 @@
 extends CharacterBody2D
 
-@export var maxSpeed : float = 2
-@export var mass : float = 2
-@export var stepFrequency : float = 10
-@export_range(0.0,1.0) var stepAnimationInfluence : float = 0.4
+@export var speed = 20.0
+var is_going_right = true
 
-
-var stepAnimationTimer = 0
-@export var max_speed = 7.0
-
-
-func _process(delta):
+func _physics_process(delta):
 	# Pobieranie wejścia z klawiatury.
 	var move_direction = Input.get_vector("left", "right", "up", "down")
 	
-	# Obliczanie prędkości, którą ma osiągnąć objekt.
-	var target_velocity = move_direction.normalized() * max_speed
+	# Ustawianie prędkości.
+	velocity = move_direction.normalized() * speed * 100
 	
-	# Siła potrzebna do skierowania postaci w kierunku target_velocity.
-	var steering_force = target_velocity - velocity
-
-	# Integracja przyspieszenia.
-	velocity += steering_force / mass * delta * 60
-	velocity.limit_length(max_speed)
-	
-	# Spraw by obiekt był skierowany w stronę kierunku ruchu.
-	self.scale.x = -abs(self.scale.x) if velocity.x <= 0 else abs(self.scale.x)
-	
-	# Integracja prędkości.
-	self.position += velocity * delta * 60
 	move_and_slide()
+	
+	# Skierowanie obiektu w strone ruchu.
+	
+	if velocity.x != 0 and (velocity.x > 0 != is_going_right):
+		is_going_right = velocity.x > 0
+		scale.x = -1
