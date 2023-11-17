86d4e7c3857f0943558ad873afc21d820d300b35
b0tinho <149424289+b0tinho@users.noreply.github.com>
2023-11-17 09:39:34 +0100

    strzelanie_1

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,8 +1,11 @@
 extends Node
 
+@onready var shoot = $".." as Shooting
 @onready var movement = $".." as Movement
 
 func _process(delta: float) -> void:
 	# Ustaw kierunek ruchu gracza na input.
 	var move_direction = Input.get_vector("left", "right", "up", "down")
 	movement.move(move_direction * movement.max_speed)
+	if Input.is_key_pressed(KEY_E):
+		shoot.shoot()
